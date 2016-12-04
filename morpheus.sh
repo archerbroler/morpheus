3f930147c6622183e15cac46fdd776c71764f56e
root <root@localhost.localdomain>
2016-12-04 00:10:38 +0000

    firewall review

--- morpheus.sh
+++ morpheus.sh
@@ -585,6 +585,7 @@ echo "${BlueF}    |  ${YellowF}http://178.21.117.152/phpmyadmin/${BlueF}        
 echo "${BlueF}    |  ${YellowF}http://malwareforensics1.blogspot.pt${BlueF}                             |"
 echo "${BlueF}    |  ${YellowF}http://www.portugalpesca.com/forum/login.php${BlueF}                     |"
 echo "${BlueF}    |  ${YellowF}telnet 216.58.214.174 [TELNET]${BlueF}                                   |"
+echo "${BlueF}    |  ${YellowF}telnet 192.168.1.254  [TELNET]${BlueF}                                   |"
 echo "${BlueF}    ╠───────────────────────────────────────────────────────────────────╝"
 sleep 1
 echo "${BlueF}    ╘ ${white}Press [${GreenF}ENTER${white}] to 'return' to main menu${RedF}!"
